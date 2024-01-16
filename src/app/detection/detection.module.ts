ed3b2601b0100abfd4824d067b1381929eb8b7bb
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-16 15:37:09 +0100

    add placeholder

--- src/app/detection/detection.module.ts
+++ src/app/detection/detection.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PredictComponent } from './predict/predict.component';
 import {RouterModule, Routes} from "@angular/router";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import {RouterModule, Routes} from "@angular/router";
   ],
   imports: [
     CommonModule,
-    RouterModule.forChild([{path: '', component: PredictComponent}])
+    RouterModule.forChild([{path: '', component: PredictComponent}]),
+    MatCardModule
   ]
 })
 export class DetectionModule { }
