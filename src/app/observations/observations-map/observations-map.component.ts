c2c4af9d36a3a1d4106c4c59539ffe507ff5eb23
Simon Egli <eglisi1@students.zhaw.ch>
2024-01-22 19:39:06 +0100

    add example use case with leaflet

--- src/app/observations/observations-map/observations-map.component.ts
+++ src/app/observations/observations-map/observations-map.component.ts
@@ -1,22 +1,51 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from "../../services/api.service";
 import {ObservationModel} from "../../model/observation.model";
+import * as L from 'leaflet';
+import {LocationService} from "../../services/location.service";
 
 @Component({
-  selector: 'app-observations-map',
-  templateUrl: './observations-map.component.html',
-  styleUrl: './observations-map.component.css'
+    selector: 'app-observations-map',
+    templateUrl: './observations-map.component.html',
+    styleUrl: './observations-map.component.css'
 })
 export class ObservationsMapComponent implements OnInit {
-  observations: ObservationModel[] = [];
+    observations: ObservationModel[] = [];
+    private map!: L.Map;
 
-  constructor(private apiService: ApiService) { }
+    constructor(private apiService: ApiService, private locationService: LocationService) {
+    }
 
-  ngOnInit(): void {
-    this.apiService.getObservations().subscribe((value) => {
-      this.observations = value;
-    }, error => {
-        console.error(error);
-    })
-  }
+    ngOnInit(): void {
+        this.initMap();
+        this.loadObservations();
+    }
+
+    loadObservations(): void {
+        this.apiService.getObservations().subscribe((value) => {
+            this.observations = value;
+            this.addObservationsToMap();
+        }, error => {
+            console.error(error);
+        });
+    }
+
+    initMap(): void {
+        this.locationService.getCurrentLocation().then(data => {
+            this.map = L.map('map').setView([data.latitude, data.longitude], 15);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '© OpenStreetMap contributors'
+            }).addTo(this.map);
+        });
+    }
+
+    private addObservationsToMap(): void {
+        this.observations.forEach(observation => {
+            L.marker([observation.latitude, observation.longitude])
+                .addTo(this.map)
+                .bindPopup(`Prediction: ${observation.prediction}`);
+        });
+    }
 }
